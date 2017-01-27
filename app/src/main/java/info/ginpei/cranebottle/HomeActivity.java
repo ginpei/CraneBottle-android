603cb16d15d519d84dc7312541d76c90e6243c10
ginpei <ginpei@gmail.com>
2017-01-26 22:49:30 -0800

    status text

--- app/src/main/java/info/ginpei/cranebottle/HomeActivity.java
+++ app/src/main/java/info/ginpei/cranebottle/HomeActivity.java
@@ -2,12 +2,39 @@ package info.ginpei.cranebottle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView statusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        statusTextView = (TextView) findViewById(R.id.textView_status);
+
+        findViewById(R.id.button_start).setOnClickListener(view -> {
+            String text = getString(R.string.start);
+            setStatusText(text);
+            toast(text);
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setStatusText("Hello World!");
+    }
+
+    private void setStatusText(String text) {
+        statusTextView.setText(text);
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
